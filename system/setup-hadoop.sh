199cf177400ed674511e8aa8c1ee32837bf8b3a1
gulyasm <mgulyas86@gmail.com>
2018-06-17 16:20:11 +0200

    adds maven install

--- system/setup-hadoop.sh
+++ system/setup-hadoop.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update
 sudo apt-get install -y openjdk-8-jdk-headless
-sudo apt-get install -y htop git curl
+sudo apt-get install -y htop git curl maven
 wget http://xenia.sote.hu/ftp/mirrors/www.apache.org/hadoop/common/hadoop-2.8.4/hadoop-2.8.4.tar.gz
 tar -xf hadoop-2.8.4.tar.gz
 sudo mv hadoop-2.8.4 /opt/
